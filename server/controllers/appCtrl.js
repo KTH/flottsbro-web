e0c7bc8aa0ccc07a93e0c7efb42c0a1ebebe4ac7
Patric Jansson <paddy@kth.se>
2018-05-04 23:01:41 +0200

    evn bttr

--- server/controllers/appCtrl.js
+++ server/controllers/appCtrl.js
@@ -1,49 +1,39 @@
-"use strict";
+'use strict'
 
-const api = require("../api");
-const co = require("co");
-const log = require("kth-node-log");
+const api = require('../api')
+const co = require('co')
+const log = require('kth-node-log')
 const {
   safeGet
-} = require("safe-utils");
+} = require('safe-utils')
 
 module.exports = {
   getIndex: co.wrap(getIndex)
-};
+}
 
 function* getIndex(req, res, next) {
   try {
-    console.log(`--------> ${api}`)
-    const paths = api.pipelineApi.paths;
-    console.log(`Paths: ${paths}`);
+    const client = api.pipelineApi.client
+    const paths = api.pipelineApi.paths
+    const resp = yield client.getAsync(client.resolve(paths.getLatestByClusterName.uri, {
+      'clusterName': 'stage'
+    }), {
+      useCache: true
+    })
 
-    const client = api.pipelinapieApi.client;
-
-    const resp = yield client.getAsync(
-      client.resolve(paths.getLatestByClusterName.uri, {
-        clusterName: "stage"
-      }), {
-        useCache: true
-      }
-    );
-
-    res.render("sample/index", {
-      debug: "debug" in req.query,
-      data: resp.statusCode === 200 ?
-        safeGet(() => {
-          return resp.body.name;
-        }) : "",
-      error: resp.statusCode !== 200 ?
-        safeGet(() => {
-          return resp.body.message;
-        }) : ""
-    });
+    res.render('index/index', {
+      debug: 'debug' in req.query,
+      data: resp.statusCode === 200 ? safeGet(() => {
+        return resp.body.name
+      }) : '',
+      error: resp.statusCode !== 200 ? safeGet(() => {
+        return resp.body.message
+      }) : ''
+    })
   } catch (err) {
-    console.log("---------------------------------------------------------");
-    log.error("Error in getIndex", {
+    log.error('Error in getIndex', {
       error: err
-    });
-    next(err);
-    console.log("---------------------------------------------------------");
+    })
+    next(err)
   }
 }
