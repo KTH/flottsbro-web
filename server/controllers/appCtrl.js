ba4ad56a72492bd640fe3a3898e869357adee3d0
Patric Jansson <paddy@kth.se>
2019-09-09 12:40:32 +0200

    removed unused code

--- server/controllers/appCtrl.js
+++ server/controllers/appCtrl.js
@@ -5,13 +5,11 @@ const co = require("co");
 const log = require("kth-node-log");
 
 function getClusterName() {
-
   if (process.env.DISPLAY_APPS_IN_CLUSTER) {
     return process.env.DISPLAY_APPS_IN_CLUSTER;
   }
 
   return "active";
-
 }
 
 function* getIndex(req, res, next) {
@@ -20,34 +18,31 @@ function* getIndex(req, res, next) {
     const paths = api.pipelineApi.paths;
     const uri = client.resolve(paths.getLatestByClusterName.uri, {
       clusterName: getClusterName()
-    })
+    });
 
-    const deployments = yield client.getAsync(uri)
-      .then((response) => {
-
-        if (response.statusCode == 200) {
-          res.render("index/index", {
-            debug: "debug" in req.query,
-            data: response.body
-          });
-        } else {
-          res.render("index/index", {
-            debug: "debug" in req.query,
-            error: "We are currently not able to show information about applications :("
-          })
-        }
-
-      });
-
+    client.getAsync(uri).then(response => {
+      if (response.statusCode == 200) {
+        res.render("index/index", {
+          debug: "debug" in req.query,
+          data: response.body
+        });
+      } else {
+        res.render("index/index", {
+          debug: "debug" in req.query,
+          error:
+            "We are currently not able to show information about applications :("
+        });
+      }
+    });
   } catch (err) {
     log.error("Unable to render deployments from the API.", {
       error: err
     });
     res.render("index/index", {
       debug: "debug" in req.query,
-      error: "We are currently not able to show information about applications :("
-    })
-
+      error:
+        "We are currently not able to show information about applications :("
+    });
   }
 }
 
