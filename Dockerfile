5fa65a0e4c2cc303b8a06b9763e2439a9d389644
Patric Jansson <paddy@kth.se>
2019-10-29 12:43:18 +0100

    nu?

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,13 @@ COPY ["i18n", "i18n"]
 COPY ["gulpfile.js", "gulpfile.js"]
 COPY ["package-lock.json", "package-lock.json"]
 COPY ["package.json", "package.json"]
-RUN npm run docker
 
 # Copy source files, so changes does not trigger gulp.
 COPY ["config", "config"]
 COPY ["app.js", "app.js"]
 COPY ["server", "server"]
+RUN npm run docker
 
 EXPOSE 3000
 
-CMD ["node", "app.js"]
+CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -3,11 +3,6 @@
 const nodeEnv = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase();
 if (nodeEnv === "development" || nodeEnv === "dev" || !nodeEnv) {
   require("dotenv").config();
-} else if (!process.env.SERVICE_PUBLISH) {
-  // This is an ANSIBLE machine which doesn't set env-vars atm
-  // so read localSettings.js which we now use to fake env-vars
-  // because it already exists in our Ansible setup.
-  require("./config/localSettings");
 }
 
 const config = require("./server/configuration").server;
