bfb2a922029edfe97bfdc51cac614f1ad60169a7
Patric Jansson <paddy@kth.se>
2020-11-09 17:45:13 +0100

    pkg updates

---
+++ tests/unit-tests/test-app.js
@@ -0,0 +1,20 @@
+/* eslint-env mocha */
+"use strict";
+
+// Testing libraries
+const expect = require("chai").expect;
+
+describe("Place tests here\n", function () {
+  it("Test 1.", function () {
+    expect(true).to.be.true;
+  });
+  it("Test 2.", function () {
+    expect(true).to.be.true;
+  });
+  it("Test 3.", function () {
+    expect(true).to.be.true;
+  });
+  it("Test 4.", function () {
+    expect(true).to.be.true;
+  });
+});
